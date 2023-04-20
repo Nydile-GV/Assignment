b0a8a0a13ee5488f104f8da8ab9211fb2b6a6e0d
Nydile GV <nydilegv97@gmail.com>
2023-04-20 11:56:44 +0530

    CodeUpdated

---
+++ Providers.tf
@@ -0,0 +1,14 @@
+
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
